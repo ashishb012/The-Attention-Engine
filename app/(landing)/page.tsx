0eed6388ecd4474e248861aeb815a67b6213663f
ashishb012 <ashishb0128@gmail.com>
2024-01-13 22:30:40 +0530

    clerk auth

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
 const LandingPage = () => {
   return (
     <div>
       <p>Landing Page</p>
+      <UserButton afterSignOutUrl="/"></UserButton>
     </div>
   );
 };
