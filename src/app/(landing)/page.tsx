e8ae2d93443a3513b66ddd0f9accf11b8fcee369
ashishb012 <ashishb0128@gmail.com>
2024-01-14 15:22:03 +0530

    src folder added

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,8 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
 const LandingPage = () => {
   return (
     <div>
       <p>Landing Page</p>
+      <Button>C</Button>
       <UserButton afterSignOutUrl="/"></UserButton>
     </div>
   );
